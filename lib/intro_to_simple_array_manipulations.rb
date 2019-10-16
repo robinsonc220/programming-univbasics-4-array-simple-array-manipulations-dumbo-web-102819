2888506d9e4136e0e89b1772e21db0d88c609a6f
Conrad Robinson <robinsonc220@gmail.com>
2019-10-16 17:22:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,5 +23,18 @@ def shift_with_args(array)
 end
 
 def using_concat(array1, array2)
-array1.concat(array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+    array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array = [[array2]]
+  array.flatten
 end
